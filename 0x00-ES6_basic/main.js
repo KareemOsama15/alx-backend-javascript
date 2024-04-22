fcb8f22617a8fb30dbd17003c4183f17a49a75d8
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-04-23 01:09:53 +0200

    [push] project tasks[10]

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -65,7 +65,13 @@
 
 // task 9
 
-import getFullBudgetObject from './9-getFullBudget.js';
-const fullBudget = getFullBudgetObject(20, 50, 10);
-console.log(fullBudget.getIncomeInDollars(fullBudget.income));
-console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+// import getFullBudgetObject from './9-getFullBudget.js';
+// const fullBudget = getFullBudgetObject(20, 50, 10);
+// console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+// console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+
+
+// task 10
+
+import appendToEachArrayValue from './10-loops.js';
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
