ac6ebf1f07c7bc584a84e2c20296585b3aff7640
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-04-22 22:59:56 +0200

    [push] project tasks[0:14]

---
+++ 0x00-ES6_basic/main.js
@@ -0,0 +1,11 @@
+// task 0
+
+// import { taskFirst, taskNext } from './0-constants.js';
+// console.log(`${taskFirst()} ${taskNext()}`);
+
+
+// task 1
+
+import taskBlock from './1-block-scoped.js';
+console.log(taskBlock(true));
+console.log(taskBlock(false));
