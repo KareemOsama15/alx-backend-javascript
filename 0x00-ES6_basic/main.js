c1a34e67a9a2ce02666ecfc5f9da1b7200b6c6c5
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-04-22 23:35:45 +0200

    [push] project tasks[1,2]

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,3 +1,9 @@
+// test
+
+// carName = "Volvo";
+// console.log(carName)
+// var carName;
+
 // task 0
 
 // import { taskFirst, taskNext } from './0-constants.js';
@@ -6,6 +12,14 @@
 
 // task 1
 
-import taskBlock from './1-block-scoped.js';
-console.log(taskBlock(true));
-console.log(taskBlock(false));
+// import taskBlock from './1-block-scoped.js';
+// console.log(taskBlock(true));
+// console.log(taskBlock(false));
+
+
+// task 2
+
+import getNeighborhoodsList from './2-arrow.js';
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
