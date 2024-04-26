cd581ad2145225bcd62387829b3ce04eeb141e6a
KareemOsama15 <karim.osama.saleh@gmail.com>
2024-04-26 04:50:49 +0300

    [push] project tasks[3]

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+export default class Currency {
+    constructor(code, name) {
+        if (typeof name !== 'string' || typeof code !== 'string') {
+            throw new TypeError("name or code Must be string");
+        }
+        this._name = name;
+        this._code = code;
+    }
+
+    set code(code) {
+        if (typeof code !== 'string') {
+            throw new TypeError("code must be string");
+        }
+        this._code = code;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError("name must be string");
+        }
+        this._name = name;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`
+    }
+}
--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -13,23 +13,27 @@
 
 //3
 
-import HolbertonCourse from "./2-hbtn_course";
+// import HolbertonCourse from "./2-hbtn_course";
+// const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
+// console.log(c1.name);
+// c1.name = "Python 101";
+// console.log(c1);
+// try {
+//     c1.name = 12;
+// } 
+// catch(err) {
+//     console.log(err);
+// }
+// try {
+//     const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+// }
+// catch(err) {
+//     console.log(err);
+// }
 
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
-console.log(c1.name);
-c1.name = "Python 101";
-console.log(c1);
 
-try {
-    c1.name = 12;
-} 
-catch(err) {
-    console.log(err);
-}
+// 4
 
-try {
-    const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
-}
-catch(err) {
-    console.log(err);
-}
+import Currency from "./3-currency.js";
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency());
